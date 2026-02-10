e594fd0439aa9f84adcdfb9055b48079146828c1
nitaidalal <dalalnitai7@gmail.com>
2026-02-11 00:11:46 +0530

    frontend setup done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Auth from './pages/Auth'
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/auth' element={<Auth />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/Auth.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Auth = () => {
+  return (
+    <div className="min-h-screen overflow-hidden bg-gray-100">
+      <header className="bg-white  p-4 mt-5 max-w-4xl rounded-md shadow-lg mx-auto">
+        <div className="text-center">
+          <h1 className='text-green-500 text-2xl mb-2 font-bold'>ExamNotesAi</h1>
+          <p className="text-gray-600">An AI-based exam notes generation platform</p>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Auth
